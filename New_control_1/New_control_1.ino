6d9cb8152b0b13f59ae05436a8c8aac8c26793bd
pran_d <pranavdebbad31@gmail.com>
2024-10-01 04:09:45 +0530

    Added new_control for testing

---
+++ New_control_1/New_control_1.ino
@@ -0,0 +1,263 @@
+#include "variables.h"
+
+void setup(){
+  Serial.begin(115200);
+  Wire.begin();
+  startTime = millis();
+  Wire.setClock(200000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+  IMU_initialize();
+  CAN_initialize();
+  ZeroMode(0x01);  //This is needed for when the code starts running when the motor is on and has already been used ig??s
+  EnterMode(0x01);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    mode = Serial.read();
+    switch (mode) {
+      case 'm':
+        ESP.restart();
+      case '1':
+        EnterMode(0x01);
+        break;
+      case '0':
+        ExitMode(0x01);
+        break;
+
+      case 'z':
+      {
+        float kp_extra = kp_in;
+        float kd_extra = kd_in;
+        ::p_in = 0; ::kp_in = 0; ::kd_in = 0;
+        do_each_loop(mode);
+        ZeroMode(0x01);
+        ::kd_in = kd_extra;
+        ::kp_in = kp_extra;
+      }
+      break;
+
+      case 'c':
+      {  //input gains kp, kd for mit mode pid
+        bool validInput = false;
+        while (!validInput) {
+          if (Serial.available() > 0) {
+            String input = Serial.readStringUntil('\n');
+            input.trim();
+            int spaceIndex = input.indexOf(' ');
+            Serial.println(spaceIndex);
+
+            if (spaceIndex != -1) {
+              String kp_str = input.substring(0, spaceIndex);
+              String kd_str = input.substring(spaceIndex + 1);
+
+              if (kp_str.length() > 0 && kd_str.length() > 0) {
+                ::kp_in = kp_str.toFloat();
+                ::kd_in = kd_str.toFloat();
+                Serial.printf("kp_in: %.2f, kd_in: %.2f\n", ::kp_in, ::kd_in);
+                validInput = true;
+              } else {
+                Serial.println("Invalid input. Please provide two float values separated by a space.");
+              }
+            } else {
+              Serial.println("Invalid input. Please provide two float values separated by a space.");
+            }
+          }
+        }
+        break;
+      }
+
+      case 'd':
+      {  //Input gains kp,kd,ki for external pid on i
+        bool validInput = false;
+        while (!validInput) {
+          if (Serial.available() > 0) {
+            String input = Serial.readStringUntil('\n');
+            input.trim();
+            String input_cpy = input;
+            int spaceIndex1 = input.indexOf(' ');
+            input = input.substring(spaceIndex1 + 1);
+            int spaceIndex2 = input.indexOf(' ');
+            if (spaceIndex1 != -1 && spaceIndex2 != -1) {
+              String kp_str = input_cpy.substring(0, spaceIndex1);
+              String kd_str = input.substring(0, spaceIndex2);
+              String ki_str = input.substring(spaceIndex2 + 1);
+              if (kp_str.length() > 0 && kd_str.length() > 0 && ki_str.length() > 0) {
+                kpPid = kp_str.toFloat();
+                kdPid = kd_str.toFloat();
+                kiPid = ki_str.toFloat();
+                Serial.printf("kpPid: %.3f, kdPid: %.3f,kiPid: %.3f\n", kpPid, kdPid, kiPid);
+                validInput = true;
+              } else {
+                Serial.println("Invalid input. Please provide three float values separated by spaces 1.");
+              }
+            } else {
+              Serial.println("Invalid input. Please provide three float values separated by spaces 2.");
+            }
+          }
+        }
+        break;
+      }
+
+      case 'r':  // resets torque to 0
+      {
+        ::t_in = 0;
+        break;
+      }
+
+      case 'i':  
+        {
+          if (Serial.available() > 0) {
+            String input = Serial.readStringUntil('\n');
+            float iRef = input.toFloat();
+            ::t_in = constrain(iRef, -10, 10);  // reset the torque reference to 0
+            if(::v_out < -1){
+              ::t_in = 0;
+            }
+          }
+          break;
+      }
+
+      case 'e':  // extension control - using trapezoidal velocity setpoint
+        {
+          if (Serial.available() > 0) {
+            String input = Serial.readStringUntil('\n');
+            input.trim();
+            float vRef = input.toFloat();
+            float v_max = vRef;
+            // constant velocity at the time being, but with gradual increase and gradual decrease
+            t_in = 0;            
+            float start_angle = -angleY;
+            v_in = 0;
+            float stop = 10; float margin = 15;
+            float total_rotation = -angleY-stop;
+            float rise_angle = total_rotation/3;
+            
+            while(!Serial.available()){
+            float angle_rotated = start_angle + angleY;
+      
+            ::t_in= constrain(0.15*(v_in-v_out), -3, 3);
+
+            if (angle_rotated < rise_angle) {
+                // Acceleration phase
+              ::v_in = -5 + ((v_max + 5) / rise_angle) * angle_rotated;
+            } else if (angle_rotated < (2*rise_angle)) {
+                // Constant velocity phase
+                ::v_in = v_max;
+            } else if (angle_rotated < 3*rise_angle) {
+                // Deceleration phase
+                float angle_since_decel_start = angle_rotated - 2*rise_angle;
+                ::v_in = v_max * (1 - angle_since_decel_start / rise_angle);
+
+            } else {
+                // End of motion
+                ::v_in = 0;
+                ::t_in = 0;
+                break;
+            }
+
+            if (-angleY < stop) {
+              ::v_in = 0;
+              ::t_in = 0;
+              break;
+            }
+            do_each_loop(mode); 
+            }
+      }
+      break;
+      }
+
+      case 'p':  // Unwrap to unwrap position mode. Position control. Also the walking state !!!
+        {
+          if (Serial.available() > 0) {
+            String input = Serial.readStringUntil('\n');
+            float pRef = input.toFloat();
+            ::kp_in = 5;
+            ::kd_in = 2;
+            ::p_in = pRef;
+            ::t_in = -0.3;  // reset the torque reference to 0
+          }
+          break;
+      }
+
+      case '7':  // Flexion damping to prevent buckling
+      {
+        while(!Serial.available()) {
+          ::kp_in = 5;
+          ::kd_in = 3;
+          ::p_in = 0.7;
+          ::t_in = 0;  // reset the torque reference to 0
+          if (-p_in + p_out > 0.05){
+            ::kp_in = 0;
+            ::kd_in = 0;
+            ::t_in = -1.3;
+            // Serial.println("Added Feedforward");
+          }
+          else{
+            ::t_in = -0.3;
+          }
+          do_each_loop(mode); 
+        }
+        ::kd_in = 0; ::kp_in = 0; ::t_in = 0;
+        break;
+      }
+
+      case 't':  // extension control - using trapezoidal velocity setpoint
+      {
+          if (Serial.available() > 0) {
+            String input = Serial.readStringUntil('\n');
+            input.trim();
+            float vRef = input.toFloat();
+            float v_max = vRef;
+            // constant velocity at the time being, but with gradual increase and gradual decrease
+            ::t_in = 0;            
+            ::v_in = 0;
+            
+            float p_term = 0;
+            float i_term = 0;
+            
+            int counter = 0;
+
+            while(!Serial.available()){
+              counter = counter+1;
+        
+              i_term += constrain(0.01*(::v_in-::v_out), -5, 5);
+              // p_term = 1*(v_in-v_out);
+
+              ::t_in = i_term + p_term;
+
+              if (counter < 1000) {
+                  // Acceleration phase
+                ::v_in = -5 + ((v_max + 5) / 1000) * counter;
+              } else{
+                  // Constant velocity phase
+                  ::v_in = v_max;
+              }
+              do_each_loop(mode);  
+            } 
+
+            // End of motion
+            ::v_in = 0;
+            ::t_in = 0;
+            break;
+          }
+          do_each_loop(mode); 
+          break;
+        }
+
+      case 'f':
+      {
+        if (Serial.available() > 0) {
+          String input = Serial.readStringUntil('\n');
+          input.trim();
+          float vRef = input.toFloat();
+          cable_taut(vRef);
+        }
+        break;
+      }
+
+      default:
+        break;
+    }
+  }
+  do_each_loop('M');
+}
---
+++ New_control_1/variables.h
@@ -0,0 +1,98 @@
+#include "driver/twai.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <Wire.h>
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include <esp_now.h>
+#include <WiFi.h>
+
+MPU6050 mpu;
+  
+#define ZERO_VELOCITY_BAND 0.5
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 av;         // [x, y, z]            accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+
+//MPU6050 mpu6050(Wire);
+float angleX = 0;
+float angleY = 0;
+float angleZ = 0;
+float alpha = 0.02;  // Complementary filter constant
+unsigned long previousTime, currentTime, startTime, lastMessageTime;
+
+#define RX_PIN 18
+#define TX_PIN 19
+#define TRANSMIT_RATE_MS 100
+
+#define P_MIN -12.5f
+#define P_MAX 12.5f
+#define V_MIN -45.0f
+#define V_MAX 45.0f
+#define I_MAX 18.0f
+#define I_MIN -18.0f
+#define KP_MIN 0.0f
+#define KP_MAX 500.0f
+#define KD_MIN 0.0f
+#define KD_MAX 5.0f
+
+bool logging = true;
+
+//variables  for input to motor, in pack_cmd
+float p_in = 0.0f;
+float v_in = 0.0f;
+float kp_in = 0.0f;  // kp = 5 (Pure velocity control)
+float kd_in = 0.0f;  // kd = 2 (Pure Position control)
+float t_in = 0.0f;
+
+float kpPid = 0.005;
+float kdPid = 0.15;
+float kiPid = 0.00017;
+
+//measured values - responses from Motor 1
+float p_out = 0.0f;
+float v_out = 0.0f;
+float t_out = 0.0f;
+
+const int MPU9250_ADDR = 0x68;                  // MPU9250 default I2C address
+const float RAD_TO_DEG_CUSTOM = 57.2957795131;  // Custom conversion factor from radians to degrees
+
+const float t_accel = 2000; // Acceleration time in milliseconds
+const float t_const = 2000; // Constant velocity time in milliseconds
+const float t_decel = 2000; // Deceleration time in milliseconds
+
+static bool driver_installed = false;
+unsigned long start_time; // To track the start time of the motion
+bool motion_active = false; // To track if motion is active
+
+int flag = 0;
+int counter = 0;
+
+char mode;
+
+typedef struct struct_message {
+  int time;
+  int gaitphase;
+  int fsr1;
+  int fsr2;
+  int fsr3;
+  int fsr4;
+  float kneeAngle;
+  float thighAngle;
+  float shankAngle;
+} ;
+
+struct_message sensors;
